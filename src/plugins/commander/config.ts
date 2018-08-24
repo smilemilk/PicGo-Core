64ce427609811c40ebea3513a03e2b7c4e256957
Molunerfinn <marksz@teamsz.xyz>
2018-08-24 14:41:43 +0800

    Fixed: config command

--- src/plugins/commander/config.ts
+++ src/plugins/commander/config.ts
@@ -1,17 +1,20 @@
 import PicGo from '../../core/PicGo'
 import path from 'path'
 import fs from 'fs-extra'
+import { getConfig } from '../../utils/config'
 
 export default {
   handle: (ctx: PicGo) => {
     const cmd = ctx.helper.cmd
     cmd.program
-      .option('-c, --config <path>', 'set config path', async (configPath: string) => {
-        if (!fs.pathExistsSync(configPath)) {
+      .option('-c, --config <path>', 'set config path', (configPath: string) => {
+        configPath = path.resolve(configPath)
+        if (!fs.existsSync(configPath)) {
           throw new Error('Config path doesn\'t exist!')
         }
         ctx.configPath = configPath
         ctx.baseDir = path.dirname(configPath)
+        ctx.config = getConfig(configPath).read().get('picBed').value()
       })
   }
 }
